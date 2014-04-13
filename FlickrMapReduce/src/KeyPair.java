5afb879724e4687554e6c9a62edde94a9cf20d27
ghanagayatri <gayatri.ghanakota@colorado.edu>
2014-04-13 01:18:03 -0600

    Map reduce code for local hadoop cluster-MongoDB

--- FlickrMapReduce/src/KeyPair.java
+++ FlickrMapReduce/src/KeyPair.java
@@ -1,14 +1,51 @@
 
 
-public class KeyPair {
-	  private String tag;
-	  private String location;
-	    public KeyPair(String tag, String location)
-	    {
-	        this.tag = tag;
-	        this.location = location;
 
-	    }
-	    public String getTag() { return tag; }
-	    public String getLocation() { return location; }
-}
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class KeyPair implements WritableComparable{
+	
+			 String tag;
+			 String location;
+			 
+			 public KeyPair(){
+				 
+			 }
+			 public KeyPair(String tag, String location){
+			     this.tag = tag;
+			     this.location = location;
+			 }
+			 public void write(final DataOutput out) throws IOException {
+			        out.writeUTF(this.tag);
+			        out.writeUTF(this.location);
+			    }
+			 public void readFields(DataInput in) throws IOException {
+		         tag = in.readUTF();
+		         location = in.readUTF();
+		       }
+	
+			public int compareTo(final Object o) {
+				if (!(o instanceof KeyPair)) {
+		            return -1;
+		        }
+		        KeyPair mp = (KeyPair) o;
+		        int first = tag.compareTo(mp.tag);
+		        if (first != 0) {
+		            return first;
+		        }
+		        int second = location.compareTo(mp.location);
+		        if (second != 0) {
+		            return second;
+		        }
+		        return 0;
+		    }
+	}
+    
+
+
+
--- FlickrMapReduce/src/taglocationCount.java
+++ FlickrMapReduce/src/taglocationCount.java
@@ -1,33 +1,74 @@
-import com.mongodb.hadoop.MongoConfig;
+import com.mongodb.BasicDBObjectBuilder;
+import org.bson.BasicBSONObject;
 import com.mongodb.hadoop.MongoInputFormat;
 import com.mongodb.hadoop.MongoOutputFormat;
+import com.mongodb.hadoop.io.BSONWritable;
 import com.mongodb.hadoop.util.MongoTool;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.util.ToolRunner;
+import org.bson.BSONObject;
+
+import java.io.IOException;
+
+
 
 public class taglocationCount extends MongoTool {
-	
-	public taglocationCount(){
-		Configuration conf = new Configuration();
-        MongoConfig config = new MongoConfig(conf);
-        setConf(conf);
-     
-        config.setInputFormat(MongoInputFormat.class);
-        config.setInputURI("mongodb://localhost:27017/mongo_hadoop.messages");
-        config.setMapper(mapJob.class);
-        config.setReducer(reduceJob.class);
-        config.setMapperOutputKey(KeyPair.class);
-        config.setMapperOutputValue(IntWritable.class);
-        config.setOutputKey(KeyPair.class);
-        config.setOutputValue(IntWritable.class);
-        config.setOutputURI("mongodb://localhost:27017/mongo_hadoop.message_pairs");
-        config.setOutputFormat(MongoOutputFormat.class);
-    }
 
-    public static void main(final String[] pArgs) throws Exception {
-        System.exit(ToolRunner.run(new taglocationCount(), pArgs));
-    }
+	
+
+	    static class taglocationMapper extends Mapper<Object , BSONObject, KeyPair, IntWritable> {
+
+	        @Override
+	        public void map(Object key, BSONObject value,final Context context) throws IOException, InterruptedException {
+	        	String tag = value.get("Tag").toString();
+	        	String location = value.get("location").toString();
+	            context.write(new KeyPair(tag,location), new IntWritable(1));
+	        }
+
+	    }
+
+	    static class taglocationReducer extends Reducer<KeyPair, IntWritable, BSONWritable, IntWritable> {
+
+	        @Override
+	        public void reduce(final KeyPair key, final Iterable<IntWritable> iterator, final Context pcontext) throws IOException,InterruptedException {
+	            int count = 0;
+	            for ( final IntWritable value : iterator ){
+	                count += value.get();
+	            }
+	BasicBSONObject output = new BasicBSONObject();
+	output.put("tag",key.tag);
+	output.put("location",key.location);
+	
+	          pcontext.write(new BSONWritable(output), new IntWritable(count));
+	        }
+
+	    }
+
+	    @SuppressWarnings("deprecation")
+		public int run(final String[] args) throws Exception {
+	        final Configuration conf = getConf();
+	        final Job job = new Job(conf, "tag location records Count");
+	        
+	        job.setJarByClass(taglocationCount.class);
+	        job.setReducerClass(taglocationReducer.class);
+	        job.setMapperClass(taglocationMapper.class); 
+	        job.setInputFormatClass(MongoInputFormat.class);
+	        job.setOutputFormatClass(MongoOutputFormat.class);
+	        job.setOutputKeyClass(BSONWritable.class);
+	        job.setOutputValueClass(IntWritable.class);
+	        job.setMapOutputKeyClass(KeyPair.class);
+	        job.setMapOutputValueClass(IntWritable.class);
+	        return job.waitForCompletion( true ) ? 0 : 1;
+	    }
+
+	    public static void main(final String[] args) throws Exception {
+	    	System.exit( ToolRunner.run( new taglocationCount(), args ) );
+	    }
 }
 
 
